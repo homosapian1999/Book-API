58426672ed54824ae3c5e6c80b4266464959a355
homosapian <ankitd349@gmail.com>
2023-11-02 18:57:23 +0530

    Add get all books feature

---
+++ controllers/book/getAllBooksController.js
@@ -0,0 +1,19 @@
+import Book from "../../models/Book.js";
+
+const getAllBooksController = async (req, res) => {
+  try {
+    const books = await Book.find({}).select("title author summary");
+    return res.status(200).send({
+      success: true,
+      message: "All books imported successfully",
+      books,
+    });
+  } catch (err) {
+    return res.status(500).send({
+      success: false,
+      message: "Error in getAll Books controller",
+    });
+  }
+};
+
+export default getAllBooksController;
--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
 import addBookController from "../controllers/book/addBookController.js";
+import getAllBooksController from "../controllers/book/getAllBooksController.js";
 
 const router = express.Router();
 
 router.post("/addbook", authMiddleware, addBookController);
+router.get("/getall", authMiddleware, getAllBooksController);
 
 export default router;
